f3a6cb203ba166d021488d8dd3499c968d10c88a
Puh00 <alex.phu@live.com>
2021-04-05 22:14:32 +0200

    change to Pathvariables instead of multiple addresses

--- backend/src/main/java/net/puh/springboot/controller/LoadoutController.java
+++ backend/src/main/java/net/puh/springboot/controller/LoadoutController.java
@@ -3,10 +3,7 @@ package net.puh.springboot.controller;
 import net.puh.springboot.model.Loadout;
 import net.puh.springboot.service.LoadoutService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -15,24 +12,13 @@ public class LoadoutController {
 
   @Autowired LoadoutService loadoutService;
 
-  @GetMapping("random-loadout")
-  public Loadout getRandomLoadout() {
-    return loadoutService.getRandomLoadout();
-  }
-
-  @GetMapping("random-loadout-full")
-  public Loadout getRandomFullLoadout() {
-    return loadoutService.getRandomFullLoadout();
-  }
-
-  // For testing purposes, will use Pathvariables instead
-  @GetMapping("random-loadout-quart")
-  public Loadout getRandomQuartermasterLoadout() {
-    return loadoutService.getRandomQuarterMasterLoadout();
-  }
-
-  @GetMapping("random-loadout-full-quart")
-  public Loadout getRandomFullQuartermasterLoadout() {
-    return loadoutService.getRandomFullQuarterMasterLoadout();
+  @GetMapping("random/{quart}/{full}")
+  public Loadout getRandomLoadout(@PathVariable boolean quart, @PathVariable boolean full) {
+    // check if the quartermaster trait is on, if a full loadout is required and act accordingly
+    if (quart)
+      return full
+          ? loadoutService.getRandomFullQuarterMasterLoadout()
+          : loadoutService.getRandomQuarterMasterLoadout();
+    else return full ? loadoutService.getRandomFullLoadout() : loadoutService.getRandomLoadout();
   }
 }
