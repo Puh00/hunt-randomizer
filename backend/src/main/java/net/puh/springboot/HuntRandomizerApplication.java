3eadb2446297588741ad9367d613266d4b2ff6b5
Puh00 <alex.phu@live.com>
2021-04-05 12:34:39 +0200

    init backend repo

---
+++ backend/src/main/java/net/puh/springboot/HuntRandomizerApplication.java
@@ -0,0 +1,13 @@
+package net.puh.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuntRandomizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuntRandomizerApplication.class, args);
+    }
+
+}
