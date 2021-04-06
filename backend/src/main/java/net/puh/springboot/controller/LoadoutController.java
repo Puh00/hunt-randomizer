e4e3e53e4d9cf80516f366c18f688421fcf663b4
Puh00 <alex.phu@live.com>
2021-04-07 00:50:37 +0200

    allow all domains

--- backend/src/main/java/net/puh/springboot/controller/LoadoutController.java
+++ backend/src/main/java/net/puh/springboot/controller/LoadoutController.java
@@ -5,7 +5,7 @@ import net.puh.springboot.service.LoadoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/")
 public class LoadoutController {
